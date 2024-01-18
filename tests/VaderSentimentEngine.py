150a9db1dd7e2ad9a7dc22368a92be85ba499632
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-01-19 06:16:27 +0900

    Updated the VaderSentimentEngine tests and made changes to VaderSentimentEngine such that the _calculate_confidence() and _prepare() methods pass all tests.

--- tests/VaderSentimentEngine.py
+++ tests/VaderSentimentEngine.py
@@ -2,6 +2,7 @@ import pytest
 from unittest import mock
 import bittensor as bt 
 from llm_defender.core.miners.engines.prompt_injection.vader_sentiment import VaderSentimentEngine
+import json 
 
 class TestVaderSentimentEngine:
 
@@ -9,46 +10,66 @@ class TestVaderSentimentEngine:
         # Tests with default tolerance 
         engine = VaderSentimentEngine()
 
+        print("Testing for tolerance 0.0.\n")
+        print("Testing for valid compound sentiment score.")
         # Test with valid compound sentiment score
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': 0.5}
         assert engine._calculate_confidence() == 0.0
+        print("Test successful.")
 
+        print("Testing with compound sentiment score below tolerance.")
         # Test with compound sentiment score below tolerance
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': -0.5}
         assert engine._calculate_confidence() == 1.0
+        print("Test successful.")
 
+        print("Testing with invalid compound sentiment score.")
         # Test with invalid compound sentiment score
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': 1.5}
         with pytest.raises(ValueError):
             engine._calculate_confidence()
+        print("Test successful.")
 
+        print("Testing with invalid results.")
         # Test with invalid results
         engine.output = {'outcome': 'NoVaderSentiment'}
         assert engine._calculate_confidence() == 0.5
+        print("Test successful.")
 
+        print("\nNow adjusting tolerance to -0.5.\n")
         # Tests with adjusted tolerance
         engine = VaderSentimentEngine(compound_sentiment_tol=-0.5)
 
+        print("Testing with compound sentiment score equal to the adjusted tolerance, which should yield 0 (< is used, not <=).")
         # Test with compound sentiment score equal to the adjusted tolerance, which should yield 0 (< is used, not <=)
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': -0.5}
         assert engine._calculate_confidence() == 0.0
+        print("Test successful.")
 
+        print("Testing with compound sentiment score below adjusted tolerance.")
         # Test with compound sentiment score below adjusted tolerance
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': -0.51}
         assert engine._calculate_confidence() == 1.0        
+        print("Test successful.")
 
+        print("Testing with compound sentiment score equal to default tolerance (should yield 0.0).")
         # Test with compound sentiment score equal to default tolerance (should yield 0.0)
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': -0.0}
         assert engine._calculate_confidence() == 0.0
+        print("Test successful.")
 
+        print("Testing for invalid compound sentiment score.")
         # Test with invalid compound sentiment score
         engine.output = {'outcome': 'VaderSentiment', 'compound_sentiment_score': -1.5}
         with pytest.raises(ValueError):
             engine._calculate_confidence()
+        print("Test successful.")
 
+        print("Testing for invalid results.")
         # Test with invalid results
         engine.output = {'outcome': 'NoVaderSentiment'}
         assert engine._calculate_confidence() == 0.5
+        print("Test successful.")
 
     def test_populate_data(self):
         engine = VaderSentimentEngine()
@@ -64,7 +85,10 @@ class TestVaderSentimentEngine:
                         ]
         
         for valres in valid_results:
-            assert engine._populate_data(valres) == {"outcome": "VaderSentiment", "compound_sentiment_score": 0.8}
+            print(f"Now testing for valid outcome: {valres}")
+            valid_outcome = {"outcome": "VaderSentiment", "compound_sentiment_score": valres['compound']}
+            assert engine._populate_data(valres) == valid_outcome
+            print("Test successful.")
 
         # Test with VADER output having no 'compound' key
         invalid_results = [
@@ -99,8 +123,11 @@ class TestVaderSentimentEngine:
                           ('neg', 'neu', 'pos', 'compound')
                           ]
         
+        invalid_outcome = {"outcome": "NoVaderSentiment"}
         for invres in invalid_results:
-            assert engine._populate_data(invres) == {"outcome": "NoVaderSentiment"}
+            print(f"Now testing invalid outcome: {invres}")
+            assert engine._populate_data(invres) == invalid_outcome
+            print("Test successful.")
 
     def test_prepare(self):
         engine = VaderSentimentEngine()
@@ -110,35 +137,45 @@ class TestVaderSentimentEngine:
             mock.patch("os.makedirs") as mock_makedirs, \
             mock.patch("builtins.open", mock.mock_open(read_data='{"word": 1.0}')) as mock_file:
 
+            print("Testing for when the cache directory doesn't exist.")
             # Test when the cache directory doesn't exist
             mock_exists.return_value = False
             assert engine.prepare() == True
             mock_makedirs.assert_called_once()
+            print("Test successful.")
 
             # Reset mock
             mock_makedirs.reset_mock()
 
+            print("Testing for when the cache directory exists.")
             # Test when the cache directory exists
             mock_exists.return_value = True
             assert engine.prepare() == True
             mock_makedirs.assert_not_called()
+            print("Test successful.")
 
+            print("Testing reading from the custom_vader_lexicon.json file.")
             # Test reading from the custom lexicon file
             mock_file.assert_called_with(engine.lexicon_path, 'r')
             assert engine.custom_vader_lexicon == {"word": 1.0}
+            print("Test successful.")
 
+            print("Testing for the case of FileNotFoundError.")
             # Test for the case of FileNotFoundError
             mock_file.side_effect = FileNotFoundError
             with pytest.raises(FileNotFoundError):
                 engine.prepare()
+            print("Test successful.")
 
             # Reset mock for the next test
             mock_file.side_effect = None
 
+            print("Testing for the case of json.JSONDecodeError.")
             # Test for the case of json.JSONDecodeError
             mock_file.return_value = mock.mock_open(read_data='invalid json').return_value
             with pytest.raises(json.JSONDecodeError):
                 engine.prepare()
+            print("Test successful.")
 
     def test_initialize(self):
         engine = VaderSentimentEngine()
@@ -158,10 +195,14 @@ class TestVaderSentimentEngine:
             # Assert that the custom lexicon was applied to the analyzer
             # This assumes your implementation applies each lexicon key-value pair individually
             for key, value in engine.custom_vader_lexicon.items():
+                print(f"Testing that key: {key}, value: {value} in the custom VADER lexicon was applied correctly to the analyzer.")
                 assert mock_analyzer_instance.lexicon[key] == value
-
+                print("Test successful.")
+            
+            print("Testing that a SentimentIntensityAnalyzer instance is returned.")
             # Assert the method returns an analyzer instance
             assert isinstance(analyzer, SentimentIntensityAnalyzer)
+            print("Test successful.")
 
         engine = VaderSentimentEngine()
         engine.custom_vader_lexicon = {"saddest": -6.6}
@@ -171,10 +212,12 @@ class TestVaderSentimentEngine:
             mock_analyzer_instance = mock.Mock(spec=SentimentIntensityAnalyzer)
             mock_analyzer.return_value = mock_analyzer_instance
             
+            print("Testing to make sure that ValueError is raised from the lexicon value being below bounds.")
             # Test to make sure that ValueError is raised from the lexicon value being below bounds
             with pytest.raises(ValueError):
                 # Execute the initialize method
                 analyzer = engine.initialize()
+            print("Test successful.")
 
         engine = VaderSentimentEngine()
         engine.custom_vader_lexicon = {"happiest": 6.6}
@@ -184,10 +227,12 @@ class TestVaderSentimentEngine:
             mock_analyzer_instance = mock.Mock(spec=SentimentIntensityAnalyzer)
             mock_analyzer.return_value = mock_analyzer_instance
 
+            print("Testing to make sure that ValueError is raised from the lexicon value being above bounds.")
             # Test to make sure that ValueError is raised from the lexicon value being above bounds
             with pytest.raises(ValueError):
                 # Execute the initialize method
                 analyzer = engine.initialize()
+            print("Test successful.")
 
     def test_execute(self):
         engine = VaderSentimentEngine(prompt="This is a test prompt.")
@@ -206,19 +251,23 @@ class TestVaderSentimentEngine:
 
             # Execute the method
             success = engine.execute(mock_analyzer_instance)
-
+            
+            print("Testing that the execute() method was successfully executed.")
             # Assert that the method executed successfully
             assert success == True
+            print("Test successful.")
 
             # Assert that polarity_scores was called with the correct prompt
             mock_analyzer_instance.polarity_scores.assert_called_once_with(engine.prompt)
 
+            print("Testing that the engine output is correctly populated.")
             # Assert that the output is correctly populated
             expected_output = {
                 "outcome": "VaderSentiment",
                 "compound_sentiment_score": 0.5
             }
             assert engine.output == expected_output
+            print("Test successful.")
 
     def __init__(self):
         print("\nNow testing the _calculate_confidence() method:\n")
