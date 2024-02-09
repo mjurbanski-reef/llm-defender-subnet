b8dd478fe38dbab1df609d29c4dbe1c75dabfe12
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-02-10 00:06:50 +0900

    Added more tests

--- tests/miner/test_text_classification.py
+++ tests/miner/test_text_classification.py
@@ -1,21 +1,57 @@
 import pytest 
 import unittest 
-from llm_defender.core.miners.engines.prompt_injection.yara import TextClassificationEngine
+from llm_defender.core.miners.engines.prompt_injection.text_classification import TextClassificationEngine
 
 def test_init():
     
     print("\nNOW TESTING: TextClassificationEngine.__init__()\n")
 
     print("Testing that attributes correctly initialized.")
-    
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    assert text_class.prompt == "This is a test prompt."
+    assert text_class.name == "engine:text_classification"
+    print("Test successful.")
 
 def test_calculate_confidence():
 
     print("\nNOW TESTING: TextClassificationEngine._calculate_confidence()\n")
 
+    print("Testing that UNKNOWN outcome yields 0.5 output.")
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    text_class.output = {"outcome": "UNKNOWN"}
+    assert text_class._calculate_confidence() == 0.5
+    print("Test successful.")
+
+    print("Testing that SAFE outcome yields 0.0.")
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    text_class.output = {"outcome": "SAFE"}
+    assert text_class._calculate_confidence() == 0.0
+    print("Test successful.")
+
+    print("Testing that MALICIOUS yields 1.0.")
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    text_class.output = {"outcome": "MALICIOUS"}
+    assert text_class._calculate_confidence() == 1.0
+    print("Test successful.")
+
 def test_populate_data():
 
     print("\nNOW TESTING: TextClassificationEngine._populate_data()\n")
+
+    print("Testing that no results yields UNKNOWN outcome.")
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    results = []
+    assert text_class._populate_data(results) == {"outcome":"UNKNOWN"}
+    print("Test successful.")
+
+    print("Testing that valid yields valid outcome.")
+    text_class = TextClassificationEngine(prompt="This is a test prompt.")
+    results = [
+        {"label":"SAFE","score":1.0},
+        {"test":"testing","test_2":1.0}
+    ]
+    assert text_class._populate_data(results) == {"outcome":"SAFE","score":1.0}
+    print("Test successful.")
 
 def test_prepare():
 
